8c11234f8df62a11d3ab2701939d8c954d365d2f
pxlu <peter@flipdapp.co>
2019-09-09 19:59:42 -0400

    Added notes on what to gather for

---
+++ main.py
@@ -0,0 +1,14 @@
+def main():
+
+    pass
+
+    # Auth to Twitter's servers
+    # Get a dump of each user
+    # foreach user, get:
+    # @Name - Twitter Handle - String
+    # Bio information - string
+    # Who they are following - List
+    # Who is following them - List
+
+if __name__ == '__main__':
+    main()
